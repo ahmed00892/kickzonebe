013ce5b7646c5087c553a7eb7e7fc95ccdd0e63f
ahmed00892 <ahmedhassan.1142006@gmail.com>
2025-11-10 19:24:28 +0200

    Added mongoose setup and updated API index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -7,6 +8,17 @@ require("dotenv").config();
 //console.log(process.env.PORT);
 
 const port = process.env.PORT || 7000;
+
+const DB_url = process.env.DB_url;
+
+mongoose
+  .connect(DB_url)
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((error) => {
+    console.error(`Database connection error: ${error.message}`);
+  });
 
 app.use(express.json());
 
